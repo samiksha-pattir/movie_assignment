0d9b1d14217f1d9a6ecd375363917ee394f315a2
samiksha <samikshapattir21@navgurukul.org>
2023-07-08 10:51:24 +0530

    store movies details in an array object

---
+++ my-app/app/movie_data.js
@@ -0,0 +1,85 @@
+export const movie_lists = [
+    {
+        name:'Dear Zindagi',
+        year:'2016. ',
+        time:' 2h 31m ',
+        joner:'| Drama, Romance',
+        description:'Kaira is a budding cinematographer in search of a perfect life. Her encounter with Jug, an unconventional thinker, helps her gain a new perspective on life. She discovers that happiness is all about finding comfort in lifes imperfections'
+
+    },
+
+    {
+        name:'Brave',
+        year:'2012 ',
+        time:'1h 33m ',
+        joner:'| Adventurous, Comedy ',
+        description:'Determined to make her own path in life, Princess Merida defies a custom that brings chaos to her kingdom. Granted one wish, Merida must rely on her bravery and her archery skills to undo a beastly curse'
+
+    },
+
+    {
+        name:'Moana',
+        year:'2016',
+        time:'1h 47m ',
+        joner:'| Adventurous, Comedy ',
+        description:'In Ancient Polynesia, when a terrible curse incurred by the Demigod Maui reaches Moana s island, she answers the Ocean s call to seek out the Demigod to set things right.'
+
+    },
+    {
+        name:'Mulan',
+        year:'1998',
+        time:'1h 27m ',
+        joner:'| Adventurous, Comedy',
+        description:'To save her father from death in the army, a young maiden secretly goes in his place and becomes one of China greatest heroines in the process.'
+
+    },
+    {
+        name:'He Named Me Malala',
+        year:'2015',
+        time:'1h 28m ',
+        joner:'| Biography, Documentary',
+        description:'A look at the events leading up to the Taliban s attack on Pakistani schoolgirl Malala Yousafzai for speaking out on girls  education, followed by the aftermath including her speech to the United Nations.'
+
+    },
+    {
+        name:'Soul surfer',
+        year:'2011',
+        time:'1h 52m ',
+        joner:' | Drama, Biography, Family',
+        description:'Teenage surfer Bethany Hamilton overcomes the odds and her own fears of returning to the water after losing her left arm in a shark attack'
+
+    },
+    {
+        name:'Bend it like beckham',
+        year:'2002',
+        time:'1h 52m ',
+        joner:'| Comedy, Drama & Romance ',
+        description:'Two ambitious girls, despite their parents  wishes, have their hearts set on careers in professional football.'
+
+    },
+    {
+        name:'Into the wild',
+        year:'2007',
+        time:'2h 28m ',
+        joner:'| Adventure, Biography, Drama',
+        description:'After graduating from Emory University, top student and athlete Christopher McCandless abandons his possessions, gives his entire $24,000 savings account to charity and hitchhikes to Alaska to live in the wilderness. Along the way, Christopher encounters a series of characters that shape his life.'
+
+    },
+    {
+        name:'The Pursuit of Happyness',
+        year:'2006',
+        time:'1h 57m ',
+        joner:'| Drama, Biography',
+        description:'A struggling salesman takes custody of his son as he is poised to begin a life-changing professional career.'
+
+    },
+    {
+        name:'The Intouchables',
+        year:'2011',
+        time:'| 1h 52m ',
+        joner:'Drama, Biography, Drama  ',
+        description:'After he becomes a quadriplegic from a paragliding accident, an aristocrat hires a young man from the projects to be his caregiver'
+    }
+
+
+]
